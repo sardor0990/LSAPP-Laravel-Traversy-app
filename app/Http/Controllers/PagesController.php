189cc9f90eaaaedee68895eefc4310502a1a117f
sardor0990 <sardorzufarov98@gmail.com>
2021-12-02 02:15:25 +0500

    as

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -15,7 +15,6 @@ class PagesController extends Controller
     public function about(){
         $title = 'About Us';
         return view('pages.about')->with('title', $title); 
-        
     }
 
     public function services(){
