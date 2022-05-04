cc6934978311a06954b6cb969ccdb3018473c1bf
sardor0990 <sardorzufarov98@gmail.com>
2022-05-04 20:24:02 +0500

    c

--- resources/views/pages/layouts/app.blade.php
+++ resources/views/pages/layouts/app.blade.php
@@ -3,9 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
         <title>{{config('app.name', 'LSAPP')}}</title>
     </head>
     <body>
+      sdsd
       @yield('content')
     </body>
 </html>
